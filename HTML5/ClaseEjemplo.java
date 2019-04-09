915082aef3ee0a9458206749eafc966a4255e5ba
Luis M <lmmartinez@viewnext.com>
2019-04-09 18:04:11 +0200

    Java y JS

---
+++ HTML5/ClaseEjemplo.java
@@ -0,0 +1,17 @@
+
+class ClaseEjemplo
+{
+    public static void main(String[] args){
+        System.out.println("Hola Almería");
+        int variableEntera = 10;
+        float varDecimal = 30.555555555f;
+        double varDecimalPrecision = 30.999999999999999999999999999999999;
+        String texto = "Un texto";
+        boolean booleano = true;
+        System.out.println("Valor: " + variableEntera);
+        System.out.println("Valor: " + varDecimal);
+        System.out.println("Valor: " + varDecimalPrecision);
+        System.out.println("Valor: " + texto);
+        System.out.println("Valor: " + booleano);
+    }
+}
---
+++ HTML5/PruebasJS/web/js/ejemploDOM.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//document.write("<h1>Titulo</h1>");
+//document.write("<input type='button' id='button' value='Nada'/>");
+
+var h1 = document.createElement("h1");
+var texto = document.createTextNode("Titulo DOM");
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(h1);
+h1.appendChild(texto);
+
+jQuery("body").append("<h1>Titulo jQuery</h1>");
+
+document.getElementById("macro").innerHTML = "Otro texto desde JS";
+
+$("#macro").html($("#macro").html() + "Otro texto desde jQuery");
+
+/*var arrayLIs = document.getElementsByClassName("unaClase");
+for (var i = 0; i < arrayLIs.length; i++){
+    arrayLIs[i].innerHTML += " - " + i + "!!";
+}*/
+
+$( "li" ).each(function( index ) {
+  console.log( index + ": " + $( this ).text() );
+});
+
+$("unaClase").html("Cambiar valor");
